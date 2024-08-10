349a2cabe83a6ec0844ed5693192c75fac202cfe
THUISCOMPUTER <THUISCOMPUTER@example.com>
2024-08-10 03:04:49 +0200

    add components

--- client/src/pages/admin/DashboardChart.tsx
+++ client/src/pages/admin/DashboardChart.tsx
@@ -17,27 +17,64 @@ export const DashboardChart = () => {
           <BarChart
             className="Dashboard__BartChart"
             series={[{ data: [35, 44, 24, 34, 27, 50] }]}
-            height={450}
-            width={670}
+            height={290}
+            width={660}
             xAxis={[
               { data: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6"], scaleType: "band" },
             ]}
-            margin={{ top: 30, bottom: 30, left: 30, right: 30 }}
+            margin={{ top: 30, bottom: 30, left: 30, right: 55 }}
           />
         </div>
       </article>
+      {/* PieChart */}
       <article className="Dashboard__PieCharRight">
         <PieChart
+          className="Dashboard__PieChart"
           series={[
             {
-              data,
               highlightScope: { faded: "global", highlighted: "item" },
-              faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+              faded: { innerRadius: 30, additionalRadius: -10, color: "gray" },
+              data,
+              innerRadius: 30,
+              outerRadius: 100,
+              paddingAngle: 5,
+              cornerRadius: 5,
+              startAngle: -190,
+              endAngle: 180,
+              cx: 150,
+              cy: 150,
             },
           ]}
-          height={450}
-          width={400}
+          height={300}
+          width={450}
         />
+      </article>
+      {/* Data Tables */}
+      <article className="Dashboard__TableCard">
+        <table className="Dashboard__Table">
+          <thead className="">
+            <tr className="">
+              <th className="">ID</th>
+              <th className="">Title</th>
+              <th className="">Image</th>
+              <th className="">Comment</th>
+              <th className="">Date</th>
+              <th className="">ViewsPost</th>
+              <th className="">Views</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            <tr className="">
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+            </tr>
+          </tbody>
+        </table>
       </article>
     </>
   );
