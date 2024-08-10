a9b963d1ad161dfe0d79f2becc7448c4e017213e
THUISCOMPUTER <THUISCOMPUTER@example.com>
2024-08-11 01:10:32 +0200

    add posts admin form page

--- client/src/pages/admin/PostBlog.tsx
+++ client/src/pages/admin/PostBlog.tsx
@@ -1,5 +1,65 @@
 import React from "react";
+import "./PostBlog.scss";
 
 export const PostBlog = () => {
-  return <div>Post Blog</div>;
+  return (
+    <>
+      <section className="PostBlog">
+        <form
+          action="/api/v1/blogs"
+          method="post"
+          encType="multipart/form-data"
+          className="PostBlog__form"
+        >
+          <div className="PostBlog__LabelInputBox">
+            <label className="PostBlog__Label" htmlFor="title">
+              Title
+            </label>
+            <input
+              className="PostBlog__Input"
+              type="text"
+              name="title"
+              id="title"
+            />
+          </div>
+          <div className="PostBlog__LabelInputBox">
+            <label className="PostBlog__Label" htmlFor="author">
+              Author
+            </label>
+            <input
+              className="PostBlog__Input"
+              type="text"
+              name="author"
+              id="author"
+            />
+          </div>
+          <div className="PostBlog__LabelInputBox">
+            <label className="PostBlog__Label" htmlFor="description">
+              Description
+            </label>
+            <textarea
+              className="PostBlog__textarea"
+              name="description"
+              id="description"
+            ></textarea>
+          </div>
+          <div className="PostBlog__UploadBox">
+            <label htmlFor="image">Upload Image</label>
+            <div className="PostBlog__UploadImageFileBox">
+              <input
+                className="PostBlog__UploadImageFile"
+                type="file"
+                name="image"
+                id="image"
+              />
+            </div>
+          </div>
+
+          <button className="PostBlog__Submit" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+    </>
+  );
 };
